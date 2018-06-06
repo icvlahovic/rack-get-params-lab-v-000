3bcd2e512a3ec84c695805c93dbb80ff897ba62c
Ian Vlahovic <banddorkfht7@gmail.com>
2018-06-06 00:52:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,8 +14,8 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
-        resp.write "Your cart is empty" if @@cart.empty?
       end
+      resp.write "Your cart is empty" if @@cart.empty?
     elsif req.path.match(/add/)
       item_to_add = req.params["item"]
       if @@items.include? item_to_add
